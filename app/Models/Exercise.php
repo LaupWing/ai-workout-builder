a8e07a233c2c812a8517abd8cff3c4fdc13752c5
laupwing <laupwing@gmail.com>
2024-09-11 18:29:13 +0200

    Add trained_muscle_groups column to exercises and muscle_groups tables

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Exercise extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'muscle_group_id',
+        'trained_muscle_groups',
+        'name',
+        'twitter_url',
+    ];
+
+    public function muscleGroup(): BelongsTo
+    {
+        return $this->belongsTo(MuscleGroup::class);
+    }
 }
