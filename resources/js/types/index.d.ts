56c532043ae4cfe3c6eb0bee466179f020626139
laupwing <laupwing@gmail.com>
2024-09-12 20:02:27 +0200

    Refactor types/index.d.ts to include additional properties for MuscleGroup, Exercise, and WorkoutPlanSet

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -14,8 +14,40 @@ export type PageProps<
 }
 
 export interface MuscleGroup {
-    id: string
+    id: number
     name: string
     created_at: string
     updated_at: string
+}
+
+export interface Exercise {
+    id: number
+    muscle_group_id: number
+    trained_muscles: string
+    name: string
+    twitter_url: string
+    created_at: string
+    updated_at: string
+    muscle_group: MuscleGroup
+}
+
+export interface WorkoutPlanSet {
+    id: number
+    workout_plan_id: number
+    exercise_id: number
+    reps: number
+    sets: number
+    day: string
+    created_at: string
+    updated_at: string
+    exercise: Exercise
+}
+
+export interface DayWorkout {
+    exercises: WorkoutPlanSet[]
+    focus_muscle_groups: string[]
+}
+
+export interface WorkoutPlanGroupedByDay {
+    [day: string]: DayWorkout
 }
