74a6a801df6b31ca67519bbb13a5ef4b207ef451
laupwing <laupwing@gmail.com>
2024-09-14 18:40:27 +0200

    Refactor WorkoutProgram constructor to accept WorkoutPlan object

--- app/Mail/WorkoutProgram.php
+++ app/Mail/WorkoutProgram.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\WorkoutPlan;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -16,8 +17,9 @@ class WorkoutProgram extends Mailable
     /**
      * Create a new message instance.
      */
-    public function __construct()
-    {
+    public function __construct(
+        public WorkoutPlan $workoutPlan,
+    ) {
         //
     }
 
