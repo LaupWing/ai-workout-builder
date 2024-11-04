7ec9b5c197a872c79ac7498069666dc0e0a8ba91
laupwing <laupwing@gmail.com>
2024-11-04 10:39:15 +0100

    Update workout plan request message for clarity on workout days

--- routes/web.php
+++ routes/web.php
@@ -247,7 +247,7 @@ Route::post('/generate', function (GenerateWorkoutRequest $request) use ($workou
                     "role" => "user",
                     "content" => "I want to generate a workout plan for the following muscle groups: {$selectedMuscles->pluck('name')->implode(', ')}. 
                     
-                    I can only workout on the following days: {$selectedDays}. The rest should be rest days.
+                    I want to workout on the following days: {$selectedDays}. The rest should be rest days.
                     
                     I want to focus on the following muscle groups: {$focusMuscles->pluck('name')->implode(', ')}. 
                     
