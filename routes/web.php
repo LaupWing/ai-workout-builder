6bd2c630408957d33fe3bd0b9a0d4583ad808a10
laupwing <laupwing@gmail.com>
2024-11-04 10:58:24 +0100

    Remove logging of response data in web.php to streamline output

--- routes/web.php
+++ routes/web.php
@@ -306,7 +306,6 @@ Route::post('/generate', function (GenerateWorkoutRequest $request) use ($workou
         'duration_minutes_per_session' => $duration,
         'email' => $email,
     ]);
-    logger($response_data);
 
     foreach ($response_data as $day => $workoutData) {
         if ($workoutData === 'Rest day') {
